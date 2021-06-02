3c7f1634c7d17c70d8d0961229b114dfff0c59d3
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-06-02 14:07:13 -0700

    add functionality to pull posts from DB

---
+++ functions/post.js
@@ -0,0 +1,40 @@
+const { createClient } = require("@astrajs/collections")
+
+const collection = 'post'
+
+exports.handler = async function (event, context, callback) {
+    const astraClient = await createClient({
+        astraDatabaseId: process.env.ASTRA_DB_ID,
+        astraDatabaseRegion: process.env.ASTRA_DB_REGION,
+        username: process.env.ASTRA_DB_USERNAME,
+        password: process.env.ASTRA_DB_PASSWORD,
+        applicationToken: process.env.ASTRA_DB_APPLICATION_TOKEN,
+    });
+
+
+
+    // const basePath = `/api/rest/v2/KEYSPACES/${process.env.ASTRA_DBKEPSPACE}/collections/collection`;
+
+    const data = astraClient
+        .namespace(process.env.ASTRA_DB_KEYSPACE)
+        .collection(collection);
+
+    try {
+
+        // const { data, status } = await astraClient.post(basePath, {
+        //     name: "test post",
+        //   });
+        const res = await data.find({}); 
+        
+        return {
+            statusCode: 200,
+            body: JSON.stringify(res),
+    }
+} catch(e) {
+    console.error(e)
+    return {
+        statusCode: 500,
+        body: JSON.stringify(e)
+    }
+}
+}
